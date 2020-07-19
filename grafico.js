fdcd184a90fe70743a8a5b5a97d57481c1be021d
Matheus Motta <66272916+Mmotta18@users.noreply.github.com>
2020-07-19 15:01:24 -0300

    Add files via upload

--- grafico.js
+++ grafico.js
@@ -1,14 +1,15 @@
+
 function criaGrafico(idGrafico, dados, cor){
     var ctx = document.getElementById(idGrafico).getContext('2d');
-var chart = new Chart(ctx, {
+ var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'bar',
 
     // The data for our dataset
     data: {
-        labels: dados['rótulo horizontal'],
+        labels: dados["Rótulox"],
         datasets: [{
-            label: dados['indicador'],
+            label: dados["indicador"],
             backgroundColor: cor,
             borderColor: cor,
             data: dados["dados"]
@@ -17,12 +18,49 @@ var chart = new Chart(ctx, {
 
     // Configuration options go here
     options: {}
-});
+ });
 }
 
 var dadosGrafico1 = {
-    "indicador": "Quantidade de unidades (milhões) vendidas do PS4",
-    "rótulo horizontal": ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
-    "dados":[2.1, 22.3, 30, 45.2, 62.2, 73.6, 91, 110.4]
+    "indicador": "Vendas por milhão: Samsung",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[6, 5.6, 5.4, 5.6, 6.2, 6.7]
 }
-criaGrafico("grafico1", dadosGrafico1, 'rgb(0, 0, 255)')
+criaGrafico("grafico1", dadosGrafico1, 'rgb(0, 204, 0)') 
+
+var dadosGrafico2 = {
+    "indicador": "Vendas por milhão: Motorola",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[3.1, 3.2, 3.5, 3.1, 3.3, 3.8, 4]
+}
+criaGrafico("grafico2", dadosGrafico2, 'rgb(255, 0, 0)')
+
+var dadosGrafico3 = {
+    "indicador": "Vendas por milhão: Apple",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[4, 3.6, 3.5, 4.1, 3.7, 3.3]
+}
+criaGrafico("grafico3", dadosGrafico3, 'rgb(0, 255, 255)')
+
+var dadosGrafico4 = {
+    "indicador": "Vendas por milhão:  Asus",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[2.5, 2.2, 2, 2.1, 1.9, 2.1]
+}
+criaGrafico("grafico4", dadosGrafico4, 'rgb(100, 0, 100)')
+
+var dadosGrafico5 = {
+    "indicador": "Vendas por milhão: LG",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[1.8, 2, 2.2, 1.8, 1.5, 2]
+}
+criaGrafico("grafico5", dadosGrafico5, 'rgb(0, 100, 100)')
+
+var dadosGrafico6 = {
+    "indicador": "Vendas por milhão: Xiaomi",
+    "Rótulox": ['jan', 'fev', 'mar', 'abr', 'mai', 'jun'],
+    "dados":[3.1, 3.3, 3.5, 3.6, 3.8, 4.2]
+}
+criaGrafico("grafico6", dadosGrafico6, 'rgb(200, 100, 0)')
+
+
